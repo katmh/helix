8f0c550f6b6bd7276e1329d7b4d0a384b2e4b500
Katherine Huang <kat.m.huang@gmail.com>
2017-10-07 22:13:44 -0400

    Generate data (n=68) to validate DSSP helix-disparity

--- 1007_DSSP/validate.py
+++ 1007_DSSP/validate.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 
 # clean sequence dataset -> ID only
+# input file has ID and seq
 def clean(i, o):
 	clean = []
 
@@ -13,17 +14,12 @@ def clean(i, o):
 		for line in clean:
 			f.write(line + '\n')
 
-# clean('final-dataset-n68.txt', 'IDs-n68.txt')
-
 # use mkdssp executable
+# NOTE: need dompbb folder, which is very large (download from CATH)
 def generateDSSP(IDs):
 	with open(IDs) as list:
 		for ID in list.readlines(): # last char is \n
 			subprocess.call('mkdssp -i dompdb/' + ID[:-1] + ' -o dssp/' + ID[:-1] + '.dssp', shell=True)
-
-# generateDSSP('IDs-n68.txt')
-
-
 
 # put data into CSV to analyze
 def generateCSV(dssp, csv):
@@ -37,11 +33,13 @@ def generateCSV(dssp, csv):
 
 	with open(csv, 'a') as output:
 		for i, item in enumerate(ss):
-			if disp[i]:
+			if i in range(0, len(ss) - 17):
+			# why -16? TODO: look into why # of disparity values is less than expected
 				output.write(str(i) + ',' + item + ',' + disp[i] + '\n')
 			else:
 				output.write(str(i) + ',' + item + '\n')
 
+# get SS code from generated DSSP file (file name is input)
 def getSS(i):
 	secstrucs = []
 
@@ -52,15 +50,21 @@ def getSS(i):
 
 	return secstrucs
 
-
+# get disparities from previously generated file
 def getDisp(ID, disparities):
 	with open(disparities) as f:
 		for i, line in enumerate(f):
 			if line[0:7] == ID:
 				return str(next(enumerate(f))[1]).split()
 
-#print( getDisp('4fcyB02', 'disparities-n68.txt')[0] )
+# clean('final-dataset-n68.txt', 'IDs-n68.txt')
+# generateDSSP('IDs-n68.txt')
 
-#getDisp('4fcyB02', 'disparities-n68.txt')
-
-generateCSV('dssp/1qhdA02.dssp', 'csv/1qhdA02.csv')
+# generate CSV for all 68
+# NOTE: as of 10/7, it generates 65 and then list index out of range line 38
+# current goal is to analyze preliminary data, so fix this later
+with open('IDs-n68.txt') as IDs:
+	for line in IDs:
+		input = 'dssp/' + line[:-1] + '.dssp'
+		output = 'csv/' + line[:-1] + '.csv'
+		generateCSV(input, output)
